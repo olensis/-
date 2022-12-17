a7a39f7f2110d65d71ec396290735f5dc763dcc3
olensis <olensis7@gmail.com>
2022-12-17 14:29:29 +0300

    джава раш

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Main {
+    public static int width = 30;
+    public static int height = 10;
+    public static String [][] empty  = new String[height][width];
+
+
+        public static void main(String[] args) {
+            for (int i = 0; i < empty.length; i++) {
+                for (int i1 = 0; i1 < empty[height - 1].length; i1++) {
+                    empty[i][i1] = hit;
+
+                }
+            }
+
+            for (int i = 0; i < empty.length; i++) {
+                for (int i1 = 0; i1 < empty[height - 1].length; i1++) {
+
+                    System.out.print(empty[i][i1]);
+                }
+                System.out.println();
+            }
+        }
+
+    public static String robotank = "☖";
+    public static String emptyq = "_";
+    public static String hit = "🎯";
+
+    }
+
